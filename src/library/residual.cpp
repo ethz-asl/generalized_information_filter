8b8a1afb92fd5d822199eb466d7e889e6201eb50
Michael Burri <burri210@gmail.com>
2017-07-14 17:53:16 +0200

    clean up finite difference

--- src/library/residual.cpp
+++ src/library/residual.cpp
@@ -12,7 +12,6 @@ namespace tsif {
 // void ResidualBase::setMeasurementTimelines(std::vector<Timeline*> timelines)
 // { measurement_timelines_ = timelines; }
 
-// TODO(burrimi): Clean up this function!!!
 void ResidualBase::finiteDifference(
     const VectorOfBlocks& state1, const VectorOfBlocks& state2,
     const std::vector<const TimedMeasurementVector*>& measurement_vectors,
@@ -21,10 +20,6 @@ void ResidualBase::finiteDifference(
     std::vector<MatrixXRef>* jacobian_wrt_state2) {
   VectorX residual(minimal_dimension_);
   VectorX residual_perturbed(minimal_dimension_);
-  const int state1_minimal_dimension =
-      block_helper::getMinimalDimension(state1);
-  const int state2_minimal_dimension =
-      block_helper::getMinimalDimension(state2);
 
   evaluate(
       state1, state2, measurement_vectors, t1_ns, t2_ns, residual, nullptr,
@@ -32,36 +27,45 @@ void ResidualBase::finiteDifference(
 
   // Jacobians wrt state1
   if (jacobian_wrt_state1 != nullptr) {
+    const int state1_minimal_dimension =
+        block_helper::getMinimalDimension(state1);
     VectorOfBlocks state1_perturbed;
     block_helper::copyVectorOfBlocks(state1, &state1_perturbed);
 
+    VectorX delta_vector(state1_minimal_dimension);
+    delta_vector.setZero();
+    int index_to_test = 0;
+
     for (size_t block_index = 0; block_index < state1.size(); ++block_index) {
       const BlockBase::Ptr& current_block = state1[block_index];
-      VectorX delta_vector(current_block->minimal_dimension_);
 
       for (int block_offset = 0;
            block_offset < current_block->minimal_dimension_; ++block_offset) {
-        delta_vector.setZero();
-        delta_vector[block_offset] = delta;
+        delta_vector[index_to_test] = delta;
         // boxplus of relevant block
-        state1[block_index]->boxPlus(
-            delta_vector, state1_perturbed[block_index].get());
-
+        block_helper::boxPlus(
+            state1, state1_minimal_dimension, delta_vector, &state1_perturbed);
         evaluate(
             state1_perturbed, state2, measurement_vectors, t1_ns, t2_ns,
             residual_perturbed, nullptr, nullptr);
 
         (*jacobian_wrt_state1)[block_index].col(block_offset) =
             (1 / delta) * (residual_perturbed - residual);
+        delta_vector[index_to_test] = 0;  // reset delta vector
+        ++index_to_test;
       }
-      state1_perturbed[block_index] =
-          state1[block_index];  // TODO(burrimi):remove hack!
     }
   }
 
   if (jacobian_wrt_state2 != nullptr) {
+    const int state2_minimal_dimension =
+        block_helper::getMinimalDimension(state2);
     VectorOfBlocks state2_perturbed;
     block_helper::copyVectorOfBlocks(state2, &state2_perturbed);
+
+    VectorX delta_vector(state2_minimal_dimension);
+    delta_vector.setZero();
+    int index_to_test = 0;
 
     for (size_t block_index = 0; block_index < state2.size(); ++block_index) {
       const BlockBase::Ptr& current_block = state2[block_index];
@@ -69,11 +73,10 @@ void ResidualBase::finiteDifference(
 
       for (int block_offset = 0;
            block_offset < current_block->minimal_dimension_; ++block_offset) {
-        delta_vector.setZero();
-        delta_vector[block_offset] = delta;
+        delta_vector[index_to_test] = delta;
         // boxplus of relevant block
-        state2[block_index]->boxPlus(
-            delta_vector, state2_perturbed[block_index].get());
+        block_helper::boxPlus(
+            state2, state2_minimal_dimension, delta_vector, &state2_perturbed);
 
         evaluate(
             state1, state2_perturbed, measurement_vectors, t1_ns, t2_ns,
@@ -81,9 +84,10 @@ void ResidualBase::finiteDifference(
 
         (*jacobian_wrt_state2)[block_index].col(block_offset) =
             (1 / delta) * (residual_perturbed - residual);
+
+        delta_vector[index_to_test] = 0;  // reset delta vector
+        ++index_to_test;
       }
-      state2_perturbed[block_index] =
-          state2[block_index];  // TODO(burrimi):remove hack!
     }
   }
 }
